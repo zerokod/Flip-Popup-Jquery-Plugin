a363027d8a86d207e8c5d0f340fec29a77ddd3cb
zerokod <zeroblog27@gmail.com>
2014-06-01 13:40:14 +0200

    fixed 1 giugno 2014

--- jquery.flippopup.js
+++ jquery.flippopup.js
@@ -32,16 +32,31 @@
      
         var $root = $(this);
         $("body").append("<div id='fade' class='black_overlay'></div>");
+        //$("body").append("<div class='topcontainer' ></div>");
         $("body").append( "<div class='flip-container animation white_content ' ontouchstart='this.classList.toggle('hover');'><div class='flipper'><div class='back animated flip'><div class='closeX'><a  href='#'>CLOSE X</a></div></div></div></div>" );
-        $("#fade").fadeIn( "slow" );
-        $(this).parent().css( "display", "block" );
-        $( ".closeX a" ).css( "color", settings.xcolor)
-        $( ".closeX" ).click(function() {
-        $("#fade").css( "display", "none" );
-        $(this).parent().css( "display", "none" );
-        });
         $(".back").append($root);
-        $(".back").fadeIn( "slow" );
+        $("#fade").fadeIn( "slow" );
+        $(this).fadeIn( "slow" );
+        $(this).parent().css( "display", "block" );
+        $(this).css( "display", "block" );
+        $( ".closeX a" ).css( "color", settings.xcolor);
+        
+        $( ".closeX" ).click(function() {
+            //$(this).css( "display", "none" );
+            //$("#fade").css( "display", "none" );
+            //$(this).parent().css( "display", "none" );
+            //$(this).css( "display", "none" );  
+            $("#fade").hide();
+            $(this).hide();
+            $(this).parent().hide();
+            $("body").append( "<div class='flip-container animation white_content ' ontouchstart='this.classList.toggle('hover');'><div class='flipper'><div class='back animated flip'><div class='closeX'><a  href='#'>CLOSE X</a></div></div></div></div>" );
+             $(".back").append($root);
+             $(".white_content").hide();
+            $(".back").children().hide();
+            
+        });
+        
+        
         $(".back").css( "-webkit-animation", "flip "+ settings.speed+"s 1" );
         $(".back").css( "-moz-animation", "flip "+  settings.speed+"s 1" );
         $(".back").css( "-o-animation", "flip "+  settings.speed+"s 1");
@@ -50,6 +65,7 @@
         var t=(100-parseInt(settings.height))/2;
         var l=(100-parseInt(settings.width))/2;
         $(this).parent().css( "color", settings.color);
+        $( ".white_content" ).css( "position", "absolute");
         $( ".white_content" ).css( "top", t+"%");
         $( ".white_content" ).css( "left", l+"%");
         $( ".black_overlay" ).css( "background", settings.lightboxcolor);
